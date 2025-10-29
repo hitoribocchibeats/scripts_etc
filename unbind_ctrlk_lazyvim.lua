4745771219a8347c51e24a946070f4d5692c1096
hitoribocchibeats <hitoribocchibeats@gmail.com>
2025-10-29 06:20:55 -0400

    Create unbind_ctrlk_lazyvim.lua

---
+++ unbind_ctrlk_lazyvim.lua
@@ -0,0 +1,13 @@
+-- place in .config/nvim/lua/plugins
+-- name it something like lsp.lua
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+
+      -- disable signature help in insert mode
+      keys[#keys + 1] = { "<c-k>", false, mode = "i" }
+    end,
+  },
+}
